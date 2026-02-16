11e0c47745b0c3d4bd419d2aee8259859dcd9728
GITCarlosBarrera <carlos.barrera.2003@gmail.com>
2026-02-16 17:17:29 +0100

    fix: fixed ApiInterface.kt and Repositorio.kt "Bovimos" functions

--- terrabit_app/app/src/main/java/com/example/terrabit_app/data/network/ApiInterface.kt
+++ terrabit_app/app/src/main/java/com/example/terrabit_app/data/network/ApiInterface.kt
@@ -14,7 +14,7 @@ import com.example.terrabit_app.data.network.guias.PeticionAltaGuia
 import com.example.terrabit_app.data.network.guias.PeticionModificarGuia
 import com.example.terrabit_app.data.network.animales.RegistroMuerteBovi
 import com.example.terrabit_app.data.network.animales.RegistroNacimientoBovi
-import com.example.terrabit_app.data.network.guiasPorcinos.RespuestaMovilidadPorcinos
+import com.example.terrabit_app.data.network.lista_bovinos.ListaBovinos
 import com.example.terrabit_app.data.network.respuestas.ResAltaGuia
 import com.example.terrabit_app.data.network.respuestas.ResBasica
 import com.example.terrabit_app.data.network.respuestas.ResConfirmacionMovi
@@ -23,6 +23,7 @@ import com.example.terrabit_app.data.network.respuestas.RespuestaUnificada
 import com.example.terrabit_app.data.network.guiasPorcinos.GuiaMobilitatPorcinos
 import com.example.terrabit_app.data.network.guiasPorcinos.PeticionModificarGuiaPorcinos
 import com.example.terrabit_app.data.network.guiasPorcinos.ResModificarGuiaPorcinos
+import com.example.terrabit_app.data.network.guiasPorcinos.RespuestaMovilidadPorcinos
 
 import okhttp3.OkHttpClient
 import retrofit2.Response
@@ -35,6 +36,14 @@ import retrofit2.http.Query
 import java.util.concurrent.TimeUnit
 
 interface ApiInterface {
+
+    @GET("WSEnregistramentIDT/AppJava/WSConsultaAnimals/")
+    suspend fun getListaBovinos(
+        @Query("nif") nif: String,
+        @Query("password") password: String,
+        @Query("tipusVinculacio") tipusVinculacio: String,
+        @Query("explotacio") explotacio: String
+    ): Response<ListaBovinos>
     @GET("WSBovi/AppJava/Bovi/WSIdentificadorsDisponibles/")
     suspend fun getIdentificadoresDisponibles(
         @Query("nif") nif: String,
--- terrabit_app/app/src/main/java/com/example/terrabit_app/data/network/Repositorio.kt
+++ terrabit_app/app/src/main/java/com/example/terrabit_app/data/network/Repositorio.kt
@@ -23,6 +23,13 @@ class Repositorio {
         codiMO: String
     ) = apiInterface.getIdentificadoresDisponibles(nif, passwordMobilitat, codiMO)
 
+    suspend fun getListaBovinos(
+        nif: String,
+        password: String,
+        tipusVinculacio: String,
+        explotacio: String
+    ) = apiInterface.getListaBovinos(nif, password, tipusVinculacio, explotacio)
+
     suspend fun getDescargaGuiasMobilitat(
         nif: String,
         passwordMobilitat: String,
