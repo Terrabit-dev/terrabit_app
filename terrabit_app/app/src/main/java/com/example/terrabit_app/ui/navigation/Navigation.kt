10cbbee30566869dcf6af32b832392defd320df0
ITB-Carlos-Vargas <carlos.vargas.7e8@itb.cat>
2026-02-17 09:44:18 +0100

    refactor: Limpieza de imports en  Navigation.kt

--- terrabit_app/app/src/main/java/com/example/terrabit_app/ui/navigation/Navigation.kt
+++ terrabit_app/app/src/main/java/com/example/terrabit_app/ui/navigation/Navigation.kt
@@ -8,12 +8,6 @@ import androidx.navigation.compose.rememberNavController
 import com.example.terrabit_app.ui.navigation.Routes
 import com.example.terrabit_app.ui.screen.DrawerScreen
 import com.example.terrabit_app.ui.screen.Login
-import com.example.terrabit_app.ui.screen.bovinos.Movimientos
-import com.example.terrabit_app.ui.screen.porcinos.EntradasPorcinos
-import com.example.terrabit_app.ui.screen.porcinos.GestionGuiasPorcinos
-import com.example.terrabit_app.ui.screen.porcinos.HomePorcinos
-import com.example.terrabit_app.viewmodel.BorradorViewModel
-import com.example.terrabit_app.viewmodel.CorrecionSexoViewModel
 import com.example.terrabit_app.viewmodel.DrawerViewModel
 import com.example.terrabit_app.viewmodel.MainViewmodel
 
